a2c2a7c4061b45bf428c143054ffe7c8399e91f1
James Parry-Turner <jamesparryturner@gmail.com>
2019-01-11 15:50:58 +0000

    Added the endpoints for the planets controller.

--- Planets/Planets/API/PlanetsController.cs
+++ Planets/Planets/API/PlanetsController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Planets.Common.Service;
+using Planets.Data.Model;
 
 namespace Planets.API
 {
@@ -18,12 +19,68 @@ namespace Planets.API
             _planetService = planetService;
         }
 
-        [Route("planets")]
+        [HttpGet, Route("planets")]
         public HttpResponseMessage GetPlanets()
         {
             try
             {
                 var response = _planetService.GetAllPlanets();
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpGet, Route("planet")]
+        public HttpResponseMessage GetPlanet(long id)
+        {
+            try
+            {
+                var response = _planetService.GetPlanetById(id);
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpPost, Route("planet/post")]
+        public HttpResponseMessage AddNewPlanet(Planet planet)
+        {
+            try
+            {
+                var response = _planetService.AddNewPlanet(planet);
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpPut, Route("planet/put")]
+        public HttpResponseMessage UpdatePlanet(Planet planet)
+        {
+            try
+            {
+                var response = _planetService.UpdatePlanet(planet);
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpDelete, Route("planet/delete")]
+        public HttpResponseMessage DeletePlanet(Planet planet)
+        {
+            try
+            {
+                var response = _planetService.DeleteNewPlanet(planet);
                 return Request.CreateResponse(HttpStatusCode.OK, response);
             }
             catch (Exception e)
